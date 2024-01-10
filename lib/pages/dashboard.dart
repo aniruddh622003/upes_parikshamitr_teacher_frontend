7514b2b3241e422491d6b14d5d6e73fb90291350
Aarav Sharma <mailaarav2002@gmail.com>
2024-01-10 16:14:39 +0530

    Completed sheets card design

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -3,13 +3,57 @@ import 'package:flutter/material.dart';
 class Dashboard extends StatelessWidget {
   Dashboard({super.key});
 
+  List<Widget> makeBatchwiseBars(List<Map<String, dynamic>> batches) {
+    List<Widget> batchwiseBars = [];
+    for (Map<String, dynamic> batch in batches) {
+      batchwiseBars.add(
+        Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '${batch['name']}',
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '${batch['sheetsEvaluated'].toString()}/${batch['totalSheets'].toString()}',
+                  style: const TextStyle(
+                    fontSize: 20,
+                  ),
+                )
+              ],
+            ),
+            ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+              child: SizedBox(
+                height: 8,
+                child: LinearProgressIndicator(
+                  value: batch['sheetsEvaluated'] / batch['totalSheets'],
+                  backgroundColor: Colors.grey[200],
+                  valueColor: const AlwaysStoppedAnimation<Color>(Colors.blue),
+                ),
+              ),
+            ),
+            const SizedBox(height: 5),
+          ],
+        ),
+      );
+      batchwiseBars.add(const SizedBox(width: 10));
+    }
+    return batchwiseBars;
+  }
+
   final sheetsData = [
     {
-      'subjectCode': 'CSAI2036',
-      'subjectName': 'Machine Learning',
-      'dueDate': '2024-01-20',
+      'subjectCode': 'CSBD1001',
+      'subjectName': 'Big Data',
+      'dueDate': '2024-01-15',
       'batches': [
-        {'name': 'AIML B1 (NH)', 'totalSheets': 20, 'sheetsEvaluated': 10},
+        {'name': 'AIML B2 (NH)', 'totalSheets': 30, 'sheetsEvaluated': 10},
         {'name': 'AIML B3 (H)', 'totalSheets': 20, 'sheetsEvaluated': 5}
       ]
     },
@@ -19,26 +63,86 @@ class Dashboard extends StatelessWidget {
       'dueDate': '2024-01-20',
       'batches': [
         {'name': 'AIML B1 (NH)', 'totalSheets': 20, 'sheetsEvaluated': 10},
+        {'name': 'AIML B3 (H)', 'totalSheets': 20, 'sheetsEvaluated': 5},
         {'name': 'AIML B3 (H)', 'totalSheets': 20, 'sheetsEvaluated': 5}
       ]
     },
     {
-      'subjectCode': 'CSAI2036',
-      'subjectName': 'Machine Learning',
-      'dueDate': '2024-01-20',
+      'subjectCode': 'CSAI1231',
+      'subjectName': 'Deep Learning',
+      'dueDate': '2024-01-25',
       'batches': [
         {'name': 'AIML B1 (NH)', 'totalSheets': 20, 'sheetsEvaluated': 10},
         {'name': 'AIML B3 (H)', 'totalSheets': 20, 'sheetsEvaluated': 5}
       ]
     }
   ];
-
   List<Widget> makeSheetCards(List<Map<String, dynamic>> sheetsData) {
     List<Widget> sheetCards = [];
-    for (var sheetData in sheetsData) {
-      sheetCards.add();
+    sheetCards.add(const Text("Evaluate Answer Sheets",
+        style: TextStyle(fontWeight: FontWeight.bold)));
+    for (Map<String, dynamic> sheetData in sheetsData) {
+      final int daysRemaining = DateTime.parse(sheetData['dueDate'].toString())
+          .difference(DateTime.now())
+          .inDays;
+      int totalSheets = 0;
+      int sheetsEvaluated = 0;
+      for (var batch in sheetData['batches'] as List<Map<String, dynamic>>) {
+        totalSheets += batch['totalSheets'] as int;
+        sheetsEvaluated += batch['sheetsEvaluated'] as int;
+      }
+      sheetCards.add(Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 231, 233, 249),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(sheetData['subjectCode'].toString()),
+                  Text(
+                    sheetData['subjectName'].toString(),
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text('Submission in $daysRemaining days'),
+                ],
+              ),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 35, vertical: 8),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Text(
+                  '$sheetsEvaluated/$totalSheets',
+                  style: const TextStyle(color: Colors.white),
+                ),
+              )
+            ],
+          ),
+          const Divider(color: Colors.grey),
+          Column(
+              children: makeBatchwiseBars(
+                  sheetData['batches'] as List<Map<String, dynamic>>)),
+        ]),
+      ));
+      sheetCards.add(const SizedBox(height: 10));
     }
     return sheetCards;
+  }
+
+  Widget makeSheetMain(List<Map<String, dynamic>> sheetsData) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: makeSheetCards(sheetsData),
+    );
   }
 
   final Widget notificationButton = Container(
@@ -203,169 +307,7 @@ class Dashboard extends StatelessWidget {
                         ],
                       ),
                       const SizedBox(height: 10),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text("Evaluate Answer Sheets",
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                          Container(
-                            padding: const EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              color: const Color.fromARGB(255, 231, 233, 249),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Column(children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(sheetData['subjectCode'].toString()),
-                                      Text(
-                                        sheetData['subjectName'].toString(),
-                                        style: const TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      Text('Submission in $difference days'),
-                                    ],
-                                  ),
-                                  Container(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 35, vertical: 8),
-                                    decoration: BoxDecoration(
-                                      color: Colors.blue,
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
-                                    child: const Text(
-                                      '5/10',
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                  )
-                                ],
-                              ),
-                              const Divider(color: Colors.grey),
-                              Column(
-                                children: [
-                                  const Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        'Batch 1',
-                                        style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      Text(
-                                        '2/5',
-                                        style: TextStyle(
-                                          fontSize: 20,
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                  const SizedBox(height: 5),
-                                  ClipRRect(
-                                    borderRadius: const BorderRadius.all(
-                                        Radius.circular(10)),
-                                    child: SizedBox(
-                                      height: 8,
-                                      child: LinearProgressIndicator(
-                                        value: 2 / 5,
-                                        backgroundColor: Colors.grey[200],
-                                        valueColor:
-                                            const AlwaysStoppedAnimation<Color>(
-                                                Colors.blue),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Column(
-                                children: [
-                                  const Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        'Batch 1',
-                                        style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      Text(
-                                        '2/5',
-                                        style: TextStyle(
-                                          fontSize: 20,
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                  const SizedBox(height: 5),
-                                  ClipRRect(
-                                    borderRadius: const BorderRadius.all(
-                                        Radius.circular(10)),
-                                    child: SizedBox(
-                                      height: 8,
-                                      child: LinearProgressIndicator(
-                                        value: 2 / 5,
-                                        backgroundColor: Colors.grey[200],
-                                        valueColor:
-                                            const AlwaysStoppedAnimation<Color>(
-                                                Colors.blue),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Column(
-                                children: [
-                                  const Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        'Batch 1',
-                                        style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      Text(
-                                        '2/5',
-                                        style: TextStyle(
-                                          fontSize: 20,
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                  const SizedBox(height: 5),
-                                  ClipRRect(
-                                    borderRadius: const BorderRadius.all(
-                                        Radius.circular(10)),
-                                    child: SizedBox(
-                                      height: 8,
-                                      child: LinearProgressIndicator(
-                                        value: 2 / 5,
-                                        backgroundColor: Colors.grey[200],
-                                        valueColor:
-                                            const AlwaysStoppedAnimation<Color>(
-                                                Colors.blue),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ]),
-                          ),
-                        ],
-                      ),
+                      makeSheetMain(sheetsData),
                     ],
                   )),
             ),
