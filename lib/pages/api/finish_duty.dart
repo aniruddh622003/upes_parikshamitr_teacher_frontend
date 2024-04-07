3f15ebc816611640948a5c46d05206ca4dccedb3
Aarav Sharma <mailaarav2002@gmail.com>
2024-04-07 19:03:22 +0530

    Finish duty api connected

---
+++ lib/pages/api/finish_duty.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:upes_parikshamitr_teacher_frontend/pages/config.dart'
+    show serverUrl;
+
+Future<dynamic> finishDuty(String remarks) async {
+  const storage = FlutterSecureStorage();
+  final String? jwt = await storage.read(key: 'jwt');
+  final String? slotId = await storage.read(key: 'slotId');
+  final response = await http.post(
+    Uri.parse('$serverUrl/teacher/flying/finish-duty'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer $jwt',
+    },
+    body: jsonEncode({
+      'remarks': remarks,
+      'slot_id': slotId,
+    }),
+  );
+  return response;
+}
