b22b03921b7cbd55ad6fa5bd0e7491483f2596b1
Eshan Dutta <eshandutta06@gmail.com>
2024-04-09 03:46:55 +0530

    Update server URL and remove MobileScannerPlugin

--- lib/pages/config.dart
+++ lib/pages/config.dart
@@ -1,7 +1,7 @@
 // String serverUrl = "http://144.24.135.11:3000";
 // String serverUrl = "http://192.168.108.150:3001";
 // String serverUrl = "http://10.0.2.2:3001";
-String serverUrl = "https://backend.parikshamitr.tech";
-// String serverUrl = "https://backend2.parikshamitr.tech";
+//String serverUrl = "https://backend.parikshamitr.tech";
+String serverUrl = "https://backend2.parikshamitr.tech";
 
 int timerDuration = 10;
